729a502fecac1a1a218df9464c81dbe924fe8d91
wowvain-dev <79089703+wowvain-dev@users.noreply.github.com>
2023-05-09 19:05:22 +0300

    Added Romanian Locale

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,14 +1,17 @@
 import enUS from './en-US';
 import zhCN from './zh-CN';
 import trTR from './tr-TR';
+import roRO from './ro-RO';
 
-export type localeType = 'en-US' | 'zh-CN' | 'tr-TR' | 'en' | 'zh' | 'tr';
+export type localeType = 'ro-RO' | 'en-US' | 'zh-CN' | 'tr-TR' | 'ro' | 'en' | 'zh' | 'tr';
 
 export default {
   messages: {
     ['en-US']: enUS,
     ['zh-CN']: zhCN,
     ['tr-TR']: trTR,
+    ['ro-RO']: roRO,
+    ['ro']: roRO,
     ['en']: enUS,
     ['zh']: zhCN,
     ['tr']: enUS,
---
+++ src/locales/ro-RO.ts
@@ -0,0 +1,17 @@
+export default {
+  'umi.block.sketch.select': 'Selectează',
+  'umi.block.sketch.pencil': 'Creion P',
+  'umi.block.sketch.shape': 'Formă R',
+  'umi.block.sketch.text': 'Text T',
+  'umi.block.sketch.image': 'Imagine',
+  'umi.block.sketch.undo': 'Anulează',
+  'umi.block.sketch.redo': 'Reface',
+  'umi.block.sketch.clear': 'Curăță',
+  'umi.block.sketch.eraser': 'Șterge',
+  'umi.block.sketch.save': 'Salvează',
+
+  'umi.block.sketch.text.placeholder': 'Scrie ceva',
+  'umi.block.sketch.text.size.small': 'Mic',
+  'umi.block.sketch.text.size.default': 'Normal',
+  'umi.block.sketch.text.size.large': 'Mare',
+};
